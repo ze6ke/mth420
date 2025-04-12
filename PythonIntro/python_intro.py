df2471913d0566871a3aff9495cb698507ccb1a7
zeke <git@ze6ke.com>
2025-04-12 00:41:13 +0000

    finish lab 1

--- PythonIntro/python_intro.py
+++ PythonIntro/python_intro.py
@@ -1,20 +1,22 @@
 # python_intro.py
 """Python Essentials: Introduction to Python.
-<Name>
-<Class>
-<Date>
+Zeke Wander
+Mth420
+4/4/25
 """
 
 
 # Problem 1 (write code below)
-
+if __name__ == "__main__":
+    print("Hello, world!")
+# Indent with four spaces (NOT a tab).
 
 # Problem 2
 def sphere_volume(r):
     """ Return the volume of the sphere of radius 'r'.
     Use 3.14159 for pi in your computation.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    return 4/3 *3.14159 * r**3
 
 
 # Problem 3
@@ -22,7 +24,8 @@ def isolate(a, b, c, d, e):
     """ Print the arguments separated by spaces, but print 5 spaces on either
     side of b.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    print(a,b, c, sep="     ", end=" ")
+    print(d,e, end=" ")
 
 
 # Problem 4
@@ -36,7 +39,7 @@ def first_half(my_string):
         >>> first_half("ipython")
         'ipy'
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    return my_string[0:len(my_string)//2]
 
 def backward(my_string):
     """ Return the reverse of the string 'my_string'.
@@ -47,7 +50,7 @@ def backward(my_string):
         >>> backward("ipython")
         'nohtypi'
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    return my_string[::-1]
 
 
 # Problem 5
@@ -66,8 +69,15 @@ def list_ops():
         >>> list_ops()
         ['fox', 'hawk', 'dog', 'bearhunter']
     """
-    raise NotImplementedError("Problem 5 Incomplete")
-
+    l = ["bear", "ant", "cat", "dog"]
+    l.append("eagle")
+    l[2] = "fox"
+    l.pop(1)
+    l.sort(reverse=True)
+    l[l.index("eagle")]="hawk"
+    l[-1] = l[-1]+"hunter"
+    return l
+    
 
 # Problem 6
 def pig_latin(word):
@@ -79,7 +89,10 @@ def pig_latin(word):
         >>> pig_latin("banana")
         'ananabay'
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    if word[0] in "aeiou":
+        return word+"hay"
+    else:
+        return word[1:] + word[0] + "ay"
 
 
 # Problem 7
@@ -87,11 +100,22 @@ def palindrome():
     """ Find and retun the largest panindromic number made from the product
     of two 3-digit numbers.
     """
-    raise NotImplementedError("Problem 7 Incomplete")
+    def is_palindromic(n):
+        return str(n) == str(n)[::-1]
+    
+    largest_palindrome = 1
+    for i in range(900, 1000):
+        for j in range(900, 1000):
+            if is_palindromic(i*j):
+                largest_palindrome = i*j
+    
+    return largest_palindrome
+    
 
 # Problem 8
 def alt_harmonic(n):
     """ Return the partial sum of the first n terms of the alternating
     harmonic series, which approximates ln(2).
     """
-    raise NotImplementedError("Problem 8 Incomplete")
+    return sum([(-1)**(i+1)/i for i in range(1, n+1)])
+    
