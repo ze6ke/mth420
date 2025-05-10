2a335145f99ada23556844c2c6ccc1f0a22dfd52
zeke <git@ze6ke.com>
2025-05-10 04:24:28 +0000

    finish Drazin Inverse/Effective Resistance lab

--- DrazinInverse/drazin.py
+++ DrazinInverse/drazin.py
@@ -1,12 +1,26 @@
 # drazin.py
 """Volume 1: The Drazin Inverse.
-<Name>
-<Class>
-<Date>
+Zeke Wander
+MTH420
+5/9/25
 """
 
 import numpy as np
 from scipy import linalg as la
+
+
+def laplacian(A):
+    """Compute the Laplacian matrix of the adjacency matrix A,
+    as well as the second smallest eigenvalue.
+
+    Parameters:
+        A ((n,n) ndarray) adjacency matrix for an undirected weighted graph.
+
+    Returns:
+        L ((n,n) ndarray): the Laplacian matrix of A
+    """
+    D = A.sum(axis=1)    # The degree of each vertex (either axis).
+    return np.diag(D) - A
 
 
 # Helper function for problems 1 and 2.
@@ -50,7 +64,12 @@ def is_drazin(A, Ad, k):
     Returns:
         (bool) True of Ad is the Drazin inverse of A, False otherwise.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    
+    if not np.allclose(A@Ad, Ad@A): return(False)
+    if not np.allclose(np.linalg.matrix_power(A, k+1) @ Ad, np.linalg.matrix_power(A, k)): return (False)
+    if not np.allclose(Ad@A@Ad, Ad): return(False)
+    
+    return(True)
 
 
 # Problem 2
@@ -63,7 +82,17 @@ def drazin_inverse(A, tol=1e-4):
     Returns:
        ((n,n) ndarray) The Drazin inverse of A.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    (n,n)=np.shape(A)
+    (T1, Q1, k1) = la.schur(A, sort=lambda x: abs(x) > tol)
+    (T2, Q2, k2) = la.schur(A, sort=lambda x: abs(x) <= tol)
+    U = np.hstack((Q1[:,:k1], Q2[:,:k2]))
+    Uinv = la.inv(U)
+    V = Uinv@A@U
+    Z = np.zeros((n,n))
+    if k1 >= 0:
+        Z[:k1,:k1] = la.inv(V)[:k1,:k1]
+    Dinv = U@Z@Uinv
+    return (Dinv)
 
 
 # Problem 3
@@ -77,7 +106,19 @@ def effective_resistance(A):
         ((n,n) ndarray) The matrix where the ijth entry is the effective
         resistance from node i to node j.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    (n,n) = np.shape(A)
+    L = laplacian(A)
+    eye = np.eye(n)
+    r = np.zeros((n,n)) # create an empty array for the result
+    for i in range(n): #process each row
+        Lj = np.vstack((L[:i, :], eye[i,:], L[i+1:, :]))  #replace the ith row of the laplacian with the ith row of the identity matrix
+        Ljd = drazin_inverse(Lj) #calculate the drazin inverse
+        r[i,:] = np.diag(Ljd) # and save the diagonal as the effective resistance for row i
+    r[np.eye(n, dtype=bool)] = 0 # override effective resistance for nodes to themselves to be 0
+    return (r)
+    
+#Complete Lab 14: Drazin Inverse from Volume 1 Labs at https://foundations-of-applied-mathematics.github.io/Links to an external site. ,   except stop after Problem 3. (Note: see Lab 5 for description of Graph Laplacian). (Requires scipy).  
+
 
 
 # Problems 4 and 5
@@ -110,7 +151,7 @@ class LinkPredictor:
         Raises:
             ValueError: If node is not in the graph.
         """
-        raise NotImplementedError("Problem 5 Incomplete"
+        raise NotImplementedError("Problem 5 Incomplete")
 
 
     def add_link(self, node1, node2):
