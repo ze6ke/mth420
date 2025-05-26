aa8dc5c39e7df1c1f883079f2d99b741744312c3
zeke <git@ze6ke.com>
2025-05-26 23:59:26 +0000

    finish CVXPY lab

--- CVXPY_Intro/cvxpy_intro.py
+++ CVXPY_Intro/cvxpy_intro.py
@@ -1,9 +1,15 @@
 # cvxpy_intro.py
 """Volume 2: Intro to CVXPY.
-<Name>
-<Class>
-<Date>
+Zeke Wander
+Mth420
+5/23/25
 """
+
+#Do Lab 16 CVXPY in Volume2.pdf at https://foundations-of-applied-mathematics.github.io/Links to an external site. 
+
+#Problem 6 is optional. You will likely need to run `pip install cvxpy' (or if that doesn't work on your system, try `conda install -c conda-forge cvxpy').
+
+
 
 import numpy as np
 import cvxpy as cp
@@ -23,7 +29,16 @@ def prob1():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    x = cp.Variable(3, nonneg = True)
+    c = np.array([2, 1, 3])
+    objective = cp.Minimize (c.T @ x)
+
+    A = np.array([[1, 2, 0], [0, 1, -4], [-2, -10, -3]])
+    b = np.array([3, 1, -12])
+    constraints = [A @x <= b]
+    problem = cp.Problem(objective, constraints)
+    s = problem.solve()
+    return (x.value, s)
 
 
 # Problem 2
@@ -41,7 +56,14 @@ def l1Min(A, b):
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    n = A.shape[1]
+    x = cp.Variable(n, nonneg = True)
+    objective = cp.Minimize(cp.norm(x, 1))
+    constraints = [A @ x == b]
+    
+    problem = cp.Problem(objective, constraints)
+    s = problem.solve()
+    return (x.value, s)
 
 
 # Problem 3
@@ -53,7 +75,30 @@ def prob3():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    #p_1 + p_2 <= 7
+    #p_3 + p_4 <= 2
+    #p_5 + p_6 <= 4
+
+    #p_1 + p_3 + p_5 = 5
+    #p_2 + p_4 + p_6 = 8
+    
+    x = cp.Variable(6, nonneg = True)
+
+    A = np.array([[1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1]])
+    a = np.array([5, 8])
+
+    B = np.array([[1, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0], 
+                  [0, 0, 0, 0, 1, 1]])
+    b = np.array([7, 2, 4])
+
+    constraints = [A @ x == a, B @ x <= b]
+
+    c = np.array([4, 7, 6, 8, 8, 9])
+    objective = cp.Minimize(c.T @ x)
+    
+    problem = cp.Problem(objective, constraints)
+    s = problem.solve()
+    return (x.value, s)
 
 
 # Problem 4
@@ -66,8 +111,16 @@ def prob4():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+   #.5 (3x1^2 + x1x2 + x1x3 + x2^2 + x2x3 + 3x3^2 )
+    Q = np.array([[3, 2, 1],[2, 4, 2],[1, 2, 3]])
+    #3x1 + x3
+    r = np.array([3, 0, 1])
+    x = cp.Variable(3)
 
+    problem = cp.Problem(cp.Minimize(.5 * cp.quad_form(x, Q) + r.T @ x))
+
+    s = problem.solve()
+    return (x.value, s)
 
 # Problem 5
 def prob5(A, b):
@@ -83,9 +136,24 @@ def prob5(A, b):
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    #.minimize ||Ax - b||_2
+    # ||x||1 = 1
+    #x nonneg
+
+    n = A.shape[1]
+    C = np.ones((n))
+    one = np.ones((1))
+    x = cp.Variable(n, nonneg = True)
 
 
+    constraints = [C @ x == one]
+    objective = cp.Minimize(cp.norm(A@x - b, 2))
+    problem = cp.Problem(objective, constraints)
+    s = problem.solve()
+    return (x.value, s)
+
+
+#optional
 # Problem 6
 def prob6():
     """Solve the college student food problem. Read the data in the file 
